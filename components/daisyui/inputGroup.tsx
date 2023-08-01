42070fc70a84e9c6019759e5c98137301baf2b96
Nemo <eric@kercambre.com>
2023-08-01 03:16:13 +0200

    test

--- components/daisyui/inputGroup.tsx
+++ components/daisyui/inputGroup.tsx
@@ -15,7 +15,7 @@ const InputGroup = ({
     (
       <div className="form-control w-full max-w-xs">
           <label className="label">
-            <span className="label-text">{label}</span>
+            <span className="label-text">{label}</span> 
           </label>
           <input type={type} placeholder={placeholder} className="input input-bordered w-full max-w-xs" name={name} required={required} />
          
