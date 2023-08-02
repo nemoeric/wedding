f95466682d6c09577701dafb81ac0573886d5b52
Nemo <eric@kercambre.com>
2023-08-02 15:21:26 +0200

    remove ts errors

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -16,25 +16,7 @@ export async function GET(request: Request) {
 
 // GET USER FROM API BASED ON JWT COOKIE
 export async function POST(request: Request) {
-
-
   console.log("POST VERIFY RECEIVED", request.headers);
   let user = null
-  let cookie = request.headers
   return NextResponse.json({ user })
-
-
-
-
-
-  
-  if(!cookie || !cookie.value) return NextResponse.json({ user })
-  let accessToken = cookie?.value
-  try {
-    let decodedToken  = jwt.verify(accessToken, process.env.JWT_SECRET);
-    const user        = await getUserByID(decodedToken?.userId)
-    return NextResponse.json({ user })
-  } catch (error) {
-    return NextResponse.json({ user })    
-  }
 }
