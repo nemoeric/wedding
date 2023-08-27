96d51e2b6e16ea0a5d8c3cf707f24bec62f65374
Nemo <eric@kercambre.com>
2023-08-27 17:32:44 +0100

    with load

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="flex justify-center items-center h-[300px]">
+      <span className="loading loading-infinity loading-lg"></span>
+
+  </div>
+}
