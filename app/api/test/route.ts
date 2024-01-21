6d88468fed0f5985df89f21ae29366c14ab322a9
Nemo <eric@kercambre.com>
2024-01-21 16:25:02 +0100

    Add email sending functionality for attended users

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,49 @@
-import { NextResponse } from 'next/server'
+import prisma from "@/prisma/prisma";
+import { User } from "@prisma/client";
+import { NextResponse } from "next/server";
+var jwt = require("jsonwebtoken");
+import resend from "@/libs/resend";
+import ImagesAreOnline from "@/emails/imagesAreOnline";
+
 // SIGN UP VERIFY TOKEN
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "ok" })
-}
+  const users = await prisma.user.findMany();
 
+  const attended = users.filter((user) => {
+    return (
+      user.thursdayWillAttend ||
+      user.fridayWillAttend ||
+      user.saturdayWillAttend
+    );
+  });
+  const attendedNot = users.filter((user) => {
+    return (
+      !user.thursdayWillAttend ||
+      !user.fridayWillAttend ||
+      !user.saturdayWillAttend
+    );
+  });
+  const hasEmail = users.filter((user) => user.email);
+
+  const eric = users.find((user) => user.email === "eric.nemo123@gmail.com");
+  console.log(eric);
+
+  const sendEmail = async (user: User) => {
+    var token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "6h",
+    });
+    await resend.emails.send({
+      from: process.env.RESEND_FROM as string,
+      to: [user.email],
+      bcc: ["hello@nemo-stanton.fr"],
+      subject: "Photos - Mariage Nemo & Stanton",
+      react: ImagesAreOnline({
+        user,
+        url: process.env.NEXT_PUBLIC_URL + "/api/auth/verify?token=" + token,
+      }),
+    });
+  };
+  if (eric) await sendEmail(eric);
+
+  return NextResponse.json({ message: "ok", eric });
+}
