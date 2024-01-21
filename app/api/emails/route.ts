245bafc158014db105146220e93883618e428aca
Nemo <eric@kercambre.com>
2024-01-21 16:15:05 +0100

    Refactor GET function in emails route to return all users

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -5,43 +5,7 @@ var jwt = require("jsonwebtoken");
 import { ImagesAreOnline } from "@/emails/imagesAreOnline";
 import { NextApiResponse } from "next";
 
-export async function GET(request: Request, response: NextApiResponse) {
+export async function GET() {
   const users = await prisma.user.findMany({});
-  const attended = users.filter((user) => {
-    return (
-      user.thursdayWillAttend ||
-      user.fridayWillAttend ||
-      user.saturdayWillAttend
-    );
-  });
-  const attendedNot = users.filter((user) => {
-    return (
-      !user.thursdayWillAttend ||
-      !user.fridayWillAttend ||
-      !user.saturdayWillAttend
-    );
-  });
-  const hasEmail = users.filter((user) => user.email);
-
-  const eric = users.find((user) => user.email === "eric.nemo123@gmail.com");
-  console.log(eric);
-
-  const sendEmail = async (user: User) => {
-    var token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "6h",
-    });
-    await resend.emails.send({
-      from: process.env.RESEND_FROM as string,
-      to: [user.email],
-      bcc: ["hello@nemo-stanton.fr"],
-      subject: "Photos - Mariage Nemo & Stanton",
-      react: ImagesAreOnline({
-        user,
-        url: process.env.NEXT_PUBLIC_URL + "/api/auth/verify?token=" + token,
-      }),
-    });
-  };
-  if (eric) await sendEmail(eric);
-
-  return Response.json({ eric });
+  return Response.json({ users });
 }
